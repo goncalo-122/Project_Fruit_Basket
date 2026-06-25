278a62fbff1339bcf7ab38e09424381adfa86690
goncalo-122 <novagoncalo1@gmail.com>
2026-06-25 23:40:56 +0100

    Implementation of Remove Item of a specific basket Featura

--- P_Quarkus_Basket/P_Quarkus_Basket/src/main/java/org/rest/BasketResource.java
+++ P_Quarkus_Basket/P_Quarkus_Basket/src/main/java/org/rest/BasketResource.java
@@ -7,6 +7,7 @@ import jakarta.ws.rs.core.Response;
 import org.entity.BasketEntity;
 import org.service.BasketService;
 import jakarta.transaction.Transactional;
+
 import java.util.List;
 
 import org.dto.BasketDTO;
@@ -42,12 +43,29 @@ public class BasketResource {
 
     @DELETE
     @Path("/delete/{basketId}")
-    @Transactional
-    public Response deleteBasket(@PathParam("basketId") long id){
+    public Response deleteBasket(@PathParam("basketId") long id) {
         boolean deleteBasket = basketService.deleteBasket(id);
         if (deleteBasket) {
             return Response.noContent().build();
         }
         return Response.status(Response.Status.NOT_FOUND).build();
     }
+
+    @DELETE
+    @Path("/{basketId}/fruit/{fruitId}")
+    public Response removeItem(
+            @PathParam("basketId") Long basketId,
+            @PathParam("fruitId") Long fruitId) {
+
+        boolean removed = basketService.removeItemFromBasket(basketId, fruitId);
+
+        if (!removed) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("Item not found in this basket")
+                    .build();
+        }
+
+        return Response.ok("Item removed" + fruitId.toString()).build();
+    }
+
 }
--- P_Quarkus_Basket/P_Quarkus_Basket/src/main/java/org/service/BasketService.java
+++ P_Quarkus_Basket/P_Quarkus_Basket/src/main/java/org/service/BasketService.java
@@ -32,16 +32,12 @@ public class BasketService {
         FruitDTO remoteFruit = fruitClient.getFruitById(dto.fruitId);
 
 
-
         if (remoteFruit.quantity < dto.quantity) {
-            throw new IllegalArgumentException(
-                    "Stock not available. Available: " + remoteFruit.quantity
-            );
+            throw new IllegalArgumentException("Stock not available. Available: " + remoteFruit.quantity);
         }
 
         if (!remoteFruit.name.equalsIgnoreCase(dto.fruitname)) {
-            throw new IllegalArgumentException(
-                    "fruitname doesn´t match -> Name Received" + remoteFruit.name + "Name registered");
+            throw new IllegalArgumentException("fruitname doesn´t match -> Name Received" + remoteFruit.name + "Name registered");
         }
 
         int newStock = remoteFruit.quantity - dto.quantity;
@@ -96,26 +92,31 @@ public class BasketService {
             return false;
         }
         for (BasketEntity item : items) {
-
-
             FruitDTO fruit = fruitClient.getFruitById(item.fruitId);
-
-
             fruit.quantity = fruit.quantity + item.quantity;
-
-
             fruitClient.updateFruitQuantity(fruit.fruitId, fruit);
         }
-
-
         long deleted = basketRepository.delete("basketId", id);
         return deleted > 0;
-
-
-
-
     }
+
     public List<BasketEntity> getBaskets() {
         return basketRepository.listAll();
     }
+
+    @Transactional
+    public boolean removeItemFromBasket(Long basketId, Long fruitId) {
+
+        BasketEntity item = basketRepository.find("basketId = ?1 AND fruitId = ?2", basketId, fruitId).firstResult();
+
+        if (item == null) {
+            return false;
+        }
+        FruitDTO fruit = fruitClient.getFruitById(fruitId);
+        fruit.quantity += item.quantity;
+        fruitClient.updateFruitQuantity(fruitId, fruit);
+        long del = basketRepository.delete("basketId = ?1 AND fruitId = ?2", basketId, fruitId);
+        return del > 0;
+    }
+
 }
